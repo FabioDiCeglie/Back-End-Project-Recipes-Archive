4018bd8e8ce063c5830e12b3b8ef5ac51a1eac3e
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-21 19:43:37 +0200

    define the apollo server with mongoDB

---
+++ index.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const MONGODB =
+  "mongodb+srv://Test-Fabio:test-fabio@cluster0.0uyrn4v.mongodb.net/?retryWrites=true&w=majority";
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log("MongoDB Connection successful");
+  return server.listen({ port: 5000 });
+});
